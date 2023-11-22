d967b62f22df84a4a26a86e85f8af9afda831d05
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-11-22 17:24:04 -0300

    Update interfacegrafica.cpp

--- src/interfacegrafica.cpp
+++ src/interfacegrafica.cpp
@@ -1,10 +1,26 @@
 #include "interfacegrafica.h"
+#include <iostream>
 
 void InterfaceGrafica::mostrarTabuleiro(Tabuleiro& tabuleiro) {
+       tabuleiro.imprime();
 }
 
 Jogada InterfaceGrafica::capturarJogada(Jogador& jogador) {
+      int linhaOrigem, colunaOrigem, linhaDestino, colunaDestino;
+    
+    std::cout << "Jogador " << (jogador.getCor() == PRETA ? "PRETO" : "VERMELHO") << ", faça sua jogada:" << std::endl;
+    std::cout << "Linha de origem: ";
+    std::cin >> linhaOrigem;
+    std::cout << "Coluna de origem: ";
+    std::cin >> colunaOrigem;
+    std::cout << "Linha de destino: ";
+    std::cin >> linhaDestino;
+    std::cout << "Coluna de destino: ";
+    std::cin >> colunaDestino;
+
+    return Jogada(linhaOrigem, colunaOrigem, linhaDestino, colunaDestino);
 }
 
-void InterfaceGrafica::exibirMensagem(string mensagem) {
+void InterfaceGrafica::exibirMensagem(std::string mensagem) {
+       std::cout << mensagem << std::endl;
 }
