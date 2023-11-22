31be586672f41b451661cf254f1be1ac7f05d2d9
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-11-22 16:23:21 -0300

    Update tabuleiro.h

--- include/tabuleiro.h
+++ include/tabuleiro.h
@@ -10,16 +10,17 @@ using namespace std;
 
 class Tabuleiro {
 private:
-    vector<vector<Peca *>> _casas;
-    Cor _da_vez;
-    RegrasDoJogo regras; 
-    InterfaceGrafica interface; 
+    std::vector<std::vector<Peca *>> casas;
+    Cor daVez;
 
 public:
     Tabuleiro();
     ~Tabuleiro();
     void imprime();
     bool movimenta(int linha, int coluna, bool diag_esq);
+    Cor getDaVez() const;
+    Peca* getPeca(int linha, int coluna) const;
+    void realizarJogada(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino);
 };
 
 #endif
