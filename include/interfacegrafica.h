011ef9c35e23467b62e535886c13608db82900fd
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-11-22 17:52:37 -0300

    Update interfacegrafica.h

--- include/interfacegrafica.h
+++ include/interfacegrafica.h
@@ -3,12 +3,17 @@
 
 #include "tabuleiro.h"
 #include "jogador.h"
+#include <SFML/Graphics.hpp>
 
 class InterfaceGrafica {
 public:
+    InterfaceGrafica();
     void mostrarTabuleiro(Tabuleiro& tabuleiro);
     Jogada capturarJogada(Jogador& jogador);
     void exibirMensagem(std::string mensagem);
+
+private:
+    sf::RenderWindow window;
 };
 
 #endif
