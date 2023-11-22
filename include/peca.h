990fadc0b832adbc03a843797746065062430aab
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-11-22 16:45:34 -0300

    Update peca.h

--- include/peca.h
+++ include/peca.h
@@ -1,21 +1,23 @@
 #ifndef PECA_H
 #define PECA_H
-
 #include <string>
 
 using namespace std;
 
 enum Cor {
-  PRETA, VERMELHA
+    PRETA, VERMELHA
 };
 
 class Peca {
-  private:
-    Cor _cor;
-    bool _dama;
-  public:
+private:
+    Cor cor;
+    bool dama;
+
+public:
     Peca(Cor cor);
-    Cor get_cor();
+    Cor getCor() const;
+    bool isDama() const;
+    void promoverDama();
 };
 
 #endif
