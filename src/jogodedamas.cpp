4097632e615d072c73df2bfe2b805db0573c6688
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-11-22 17:34:29 -0300

    Update jogodedamas.cpp

--- src/jogodedamas.cpp
+++ src/jogodedamas.cpp
@@ -1,12 +1,29 @@
 #include "jogodedamas.h"
+#include <cstdlib>
 
 JogoDeDamas::JogoDeDamas() : jogador1(PRETA), jogador2(VERMELHA) {
-}
+   }
 
 void JogoDeDamas::iniciarJogo() {
+    interface.mostrarTabuleiro(tabuleiro);
 }
 
 void JogoDeDamas::rodadaDeJogo() {
+    Jogador& jogadorAtual = (tabuleiro.getDaVez() == PRETA) ? jogador1 : jogador2;
+    Jogada jogada = jogadorAtual.realizarJogada(tabuleiro);
+
+    if (regras.jogadaValida(tabuleiro, jogada.linhaOrigem, jogada.colunaOrigem, jogada.linhaDestino, jogada.colunaDestino)) {
+        tabuleiro.realizarJogada(jogada.linhaOrigem, jogada.colunaOrigem, jogada.linhaDestino, jogada.colunaDestino);
+        interface.mostrarTabuleiro(tabuleiro);
+
+        if (jogoTerminou()) {
+            Cor vencedor = verificarVencedor();
+            interface.exibirMensagem("Fim de Jogo! O vencedor é: " + ((vencedor == PRETA) ? "PRETA" : "VERMELHA"));
+            encerrarJogo();
+        }
+    } else {
+        interface.exibirMensagem("Jogada inválida, tente novamente.");
+    }
 }
 
 bool JogoDeDamas::jogoTerminou() {
@@ -14,7 +31,32 @@ bool JogoDeDamas::jogoTerminou() {
 }
 
 Cor JogoDeDamas::verificarVencedor() {
+    int contagemPretas = 0;
+    int contagemVermelhas = 0;
+
+    // Contar as peças restantes no tabuleiro
+    for (int linha = 0; linha < 8; linha++) {
+        for (int coluna = 0; coluna < 8; coluna++) {
+            Peca* peca = tabuleiro.getPeca(linha, coluna);
+            if (peca != nullptr) {
+                if (peca->getCor() == PRETA) {
+                    contagemPretas++;
+                } else if (peca->getCor() == VERMELHA) {
+                    contagemVermelhas++;
+                }
+            }
+        }
+    }
+
+    if (contagemPretas > contagemVermelhas) {
+        return PRETA; // Jogador com as peças pretas vence
+    } else if (contagemPretas < contagemVermelhas) {
+        return VERMELHA; // Jogador com as peças vermelhas vence
+    } else {
+        return NENHUM; // Empate
+    }
 }
 
 void JogoDeDamas::encerrarJogo() {
+        exit(0);
 }
