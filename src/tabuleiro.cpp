2a1b145fdb9a84e2d4522b690f7ccc6c3d689e81
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-12-05 18:28:57 -0300

    Update tabuleiro.cpp

--- src/tabuleiro.cpp
+++ src/tabuleiro.cpp
@@ -5,60 +5,83 @@
 #include <iostream>
 
 Tabuleiro::Tabuleiro() {
-    // Inicialize o tabuleiro aqui
-    daVez = PRETA;
+ // Inicialize o tabuleiro com peças pretas na primeira fileira e peças vermelhas na última fileira
+ for (int i = 0; i < 8; i++) {
+  for (int j = 0; j < 8; j++) {
+   if (i == 0 || i == 7) {
+    _casas[i][j] = new Peca(Cor::PRETA, i, j);
+   } else {
+    _casas[i][j] = nullptr;
+   }
+  }
+ }
+ daVez = PRETA;
 }
 
 Tabuleiro::~Tabuleiro() {
-    // Limpeza de memória, se necessário
+ // Limpeza de memória, se necessário
+ for (int i = 0; i < 8; i++) {
+  for (int j = 0; j < 8; j++) {
+   if (_casas[i][j] != nullptr) {
+    delete _casas[i][j];
+   }
+  }
+ }
 }
 
 void Tabuleiro::imprime() {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (_casas[i][j] == nullptr) {
-                std::cout << "  "; // Espaço em branco para casas vazias
-            } else if (_casas[i][j]->get_cor() == PRETA) {
-                std::cout << "P"; // Peça preta
-            } else {
-                std::cout << "V"; // Peça vermelha
-            }
-            std::cout << " "; // Espaço em branco entre as casas
-        }
-        std::cout << std::endl; // Nova linha para a próxima linha do tabuleiro
-    }
+ for (int i = 0; i < 8; i++) {
+  for (int j = 0; j < 8; j++) {
+   if (_casas[i][j] == nullptr) {
+    std::cout << " "; // Espaço em branco para casas vazias
+   } else if (_casas[i][j]->get_cor() == PRETA) {
+    std::cout << "P"; // Peça preta
+   } else {
+    std::cout << "V"; // Peça vermelha
+   }
+   std::cout << " "; // Espaço em branco entre as casas
+  }
+  std::cout << std::endl; // Nova linha para a próxima linha do tabuleiro
+ }
 }
 
-bool Tabuleiro::movimenta(int linha, int coluna, int linha_destino, int coluna_destino) {
-    if (!movimentoValido(linha, coluna, linha_destino, coluna_destino)) {
-        return false; // Movimento inválido
-    }
+bool Tabuleiro::movimentoValido(int linha, int coluna, int linha_destino, int coluna_destino) {
+ // Verifica se há uma peça na posição de origem
+ Peca* pecaOrigem = getPeca(linha, coluna);
+ if (pecaOrigem == nullptr) {
+  return false;
+ }
 
-    // Realize o movimento, movendo a peça da posição atual para a posição de destino
-    _casas[linha_destino][coluna_destino] = _casas[linha][coluna];
-    _casas[linha][coluna] = nullptr;
+ // Verifica se a peça é da cor atual
+ if (pecaOrigem->get_cor() != daVez) {
+  return false;
+ }
 
-    // Atualize a vez do próximo jogador
-    _da_vez = (_da_vez == PRETA) ? VERMELHA : PRETA;
-
-    return true; // Movimento válido
+ // Verifica se o movimento é válido de acordo com as regras do jogo
+ if (abs(linha - linha_destino) == 1 && abs(coluna - coluna_destino) == 1) {
+  // Movimento diagonal
+  if (_casas[linha_destino][coluna_destino] == nullptr) {
+   // Movimento para uma casa vazia
+   return true;
+  } else {
+   // Movimento para uma casa ocupada pelo adversário
+   return true;
+  }
+ } else if (abs(linha - linha_destino) == 2 && abs(coluna - coluna_destino) == 2) {
+  // Captura diagonal
+  if (_casas[linha_destino][coluna_destino] != nullptr && pecaOrigem->get_cor() != _casas[linha_destino][coluna_destino]->get_cor()) {
+   // Captura uma peça do adversário
+   // Verifica se a peça capturada pode ser promovida a dama
+   if (pecaOrigem->get_linha() == 0 || pecaOrigem->get_linha() == 7) {
+    pecaOrigem->promoverDama();
+   }
+   return true;
+  }
+ }
+ return false;
 }
 
-Cor Tabuleiro::getDaVez() const {
-    return daVez;
-}
+// Métodos adicionais
 
 Peca* Tabuleiro::getPeca(int linha, int coluna) const {
-    if (linha < 0 || linha >= 8 || coluna < 0 || coluna >= 8) {
-        return nullptr; // Fora dos limites do tabuleiro
-    }
-    return _casas[linha][coluna];
-}
-
-void Tabuleiro::realizarJogada(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino) {
-    // Mova a peça no tabuleiro
-    movimenta(linhaOrigem, colunaOrigem, linhaDestino, colunaDestino);
-
-    // Verifique se a peça movida deve ser promovida a dama
-    regras.promoverPecaADama(*this, linhaDestino, colunaDestino);
-}
+ if (linha < 0 || linha >= 8 || coluna < 0 || coluna >= 8) {
