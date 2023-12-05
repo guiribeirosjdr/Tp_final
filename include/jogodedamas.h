68881373985f6214ead117aa6a7ebeab09227b48
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-12-05 17:45:27 -0300

    Update jogodedamas.h

--- include/jogodedamas.h
+++ include/jogodedamas.h
@@ -6,21 +6,42 @@
 #include "regrasdojogo.h"
 #include "interfacegrafica.h"
 
+// Representa um jogo de damas
 class JogoDeDamas {
 private:
-    Tabuleiro tabuleiro;
-    Jogador jogador1;
-    Jogador jogador2;
-    RegrasDoJogo regras;
-    InterfaceGrafica interface;
+  // Tabuleiro do jogo
+  Tabuleiro tabuleiro;
+
+  // Jogador 1
+  Jogador jogador1;
+
+  // Jogador 2
+  Jogador jogador2;
+
+  // Regras do jogo
+  RegrasDoJogo regras;
+
+  // Interface gráfica
+  InterfaceGrafica interface;
 
 public:
-    JogoDeDamas();
-    void iniciarJogo();
-    void rodadaDeJogo();
-    bool jogoTerminou();
-    Cor verificarVencedor();
-    void encerrarJogo();
+  // Construtor do jogo
+  JogoDeDamas();
+
+  // Inicia o jogo
+  void iniciarJogo();
+
+  // Realiza uma rodada do jogo
+  void rodadaDeJogo();
+
+  // Verifica se o jogo terminou
+  bool jogoTerminou();
+
+  // Verifica o vencedor do jogo
+  Cor verificarVencedor();
+
+  // Encerra o jogo
+  void encerrarJogo();
 };
 
 #endif
