8ef49460773e0f9c2ac4f1def57519c3b5ba4d88
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-12-05 17:44:03 -0300

    Update jogador.h

--- include/jogador.h
+++ include/jogador.h
@@ -4,11 +4,33 @@
 #include "tabuleiro.h"
 
 class Jogador {
-private:
-    Cor cor;
+protected:
+  // Cor do jogador
+  Cor cor;
+
+  // Nome do jogador
+  std::string nome;
+
+  // Nível de habilidade do jogador
+  int nivelHabilidade;
+
 public:
-    Jogador(Cor cor);
-    Jogada realizarJogada(Tabuleiro& tabuleiro);
+  // Construtor do jogador
+  Jogador(Cor cor, std::string nome, int nivelHabilidade);
+
+  // Realiza uma jogada do jogador
+  virtual Jogada realizarJogada(Tabuleiro& tabuleiro) = 0;
+};
+
+class Humano : public Jogador {
+public:
+  Humano(Cor cor, std::string nome, int nivelHabilidade) : Jogador(cor, nome, nivelHabilidade) {}
+
+  // Realiza uma jogada do jogador humano
+  Jogada realizarJogada(Tabuleiro& tabuleiro) override {
+    // ...
+    return jogada;
+  }
 };
 
 #endif
