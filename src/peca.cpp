d216a7c789ca9b55520393eae115eec5a8374249
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-12-05 18:11:56 -0300

    Update peca.cpp

--- src/peca.cpp
+++ src/peca.cpp
@@ -1,16 +1,35 @@
 #include "peca.h"
 
-Peca::Peca(Cor cor) : cor(cor), dama(false) {
-   }
+// Representa uma peça no jogo de damas
+class Peca {
+private:
+  // A cor da peça
+  Cor cor;
 
-Cor Peca::getCor() const {
+  // O status de dama da peça
+  bool dama;
+
+public:
+  // Construtor da peça
+  Peca(Cor cor) : cor(cor), dama(false) {
+    // Inicializa a peça com a cor especificada
+  }
+
+  // Retorna a cor da peça
+  Cor getCor() const {
+    // Retorna a cor da peça
     return cor;
-}
+  }
 
-bool Peca::isDama() const {
+  // Retorna o status de dama da peça
+  bool isDama() const {
+    // Retorna o status de dama da peça
     return dama;
-}
+  }
 
-void Peca::promoverDama() {
+  // Promove a peça a dama
+  void promoverDama() {
+    // Promove a peça a dama
     dama = true;
-}
+  }
+};
