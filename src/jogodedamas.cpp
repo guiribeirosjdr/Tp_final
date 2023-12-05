5d391cfea85c83fbcb09b69ce1c6f3d9f8919df7
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-12-05 18:09:13 -0300

    Update jogodedamas.cpp

--- src/jogodedamas.cpp
+++ src/jogodedamas.cpp
@@ -1,62 +1,94 @@
 #include "jogodedamas.h"
 #include <cstdlib>
 
-JogoDeDamas::JogoDeDamas() : jogador1(PRETA), jogador2(VERMELHA) {
-   }
+// Representa um jogo de damas
+class JogoDeDamas {
+private:
+  // A interface gráfica
+  InterfaceGrafica interface;
 
-void JogoDeDamas::iniciarJogo() {
+  // O tabuleiro
+  Tabuleiro tabuleiro;
+
+  // Os jogadores
+  Jogador jogador1;
+  Jogador jogador2;
+
+public:
+  // Construtor do jogo
+  JogoDeDamas() : jogador1(PRETA), jogador2(VERMELHA) {
+    // Inicializa os jogadores com as cores preta e vermelha
+  }
+
+  // Inicia o jogo
+  void iniciarJogo() {
+    // Exibe o tabuleiro
     interface.mostrarTabuleiro(tabuleiro);
-}
+  }
 
-void JogoDeDamas::rodadaDeJogo() {
-    Jogador& jogadorAtual = (tabuleiro.getDaVez() == PRETA) ? jogador1 : jogador2;
+  // Realiza uma rodada do jogo
+  void rodadaDeJogo() {
+    // Captura a jogada do jogador atual
     Jogada jogada = jogadorAtual.realizarJogada(tabuleiro);
 
+    // Verifica se a jogada é válida
     if (regras.jogadaValida(tabuleiro, jogada.linhaOrigem, jogada.colunaOrigem, jogada.linhaDestino, jogada.colunaDestino)) {
-        tabuleiro.realizarJogada(jogada.linhaOrigem, jogada.colunaOrigem, jogada.linhaDestino, jogada.colunaDestino);
-        interface.mostrarTabuleiro(tabuleiro);
+      // Verifica se a jogada captura uma peça
+      if (tabuleiro.existePeca(jogada.linhaDestino, jogada.colunaDestino) && tabuleiro.getPeca(jogada.linhaDestino, jogada.colunaDestino)->getCor() != jogadorAtual.getCor()) {
+        // Captura a peça
+        tabuleiro.removerPeca(jogada.linhaDestino, jogada.colunaDestino);
+      }
 
-        if (jogoTerminou()) {
-            Cor vencedor = verificarVencedor();
-            interface.exibirMensagem("Fim de Jogo! O vencedor é: " + ((vencedor == PRETA) ? "PRETA" : "VERMELHA"));
-            encerrarJogo();
-        }
+      // Realiza a jogada
+      tabuleiro.realizarJogada(jogada.linhaOrigem, jogada.colunaOrigem, jogada.linhaDestino, jogada.colunaDestino);
+
+      // Verifica se o jogo terminou
+      if (jogoTerminou()) {
+        // Exibe o vencedor
+        interface.exibirMensagem("Fim de Jogo! O vencedor é: " + ((vencedor == PRETA) ? "PRETA" : "VERMELHA"));
+        encerrarJogo();
+      }
     } else {
-        interface.exibirMensagem("Jogada inválida, tente novamente.");
+      // A jogada é inválida, então exibe uma mensagem
+      interface.exibirMensagem("Jogada inválida, tente novamente.");
     }
-}
+  }
 
-bool JogoDeDamas::jogoTerminou() {
+  // Verifica se o jogo terminou
+  bool jogoTerminou() {
     return regras.jogoTerminou(tabuleiro);
-}
+  }
 
-Cor JogoDeDamas::verificarVencedor() {
+  // Verifica quem é o vencedor do jogo
+  Cor verificarVencedor() {
     int contagemPretas = 0;
     int contagemVermelhas = 0;
 
     // Contar as peças restantes no tabuleiro
     for (int linha = 0; linha < 8; linha++) {
-        for (int coluna = 0; coluna < 8; coluna++) {
-            Peca* peca = tabuleiro.getPeca(linha, coluna);
-            if (peca != nullptr) {
-                if (peca->getCor() == PRETA) {
-                    contagemPretas++;
-                } else if (peca->getCor() == VERMELHA) {
-                    contagemVermelhas++;
-                }
-            }
+      for (int coluna = 0; coluna < 8; coluna++) {
+        Peca* peca = tabuleiro.getPeca(linha, coluna);
+        if (peca != nullptr) {
+          if (peca->getCor() == PRETA) {
+            contagemPretas++;
+          } else if (peca->getCor() == VERMELHA) {
+            contagemVermelhas++;
+          }
         }
+      }
     }
 
     if (contagemPretas > contagemVermelhas) {
-        return PRETA; // Jogador com as peças pretas vence
+      return PRETA; // Jogador com as peças pretas vence
     } else if (contagemPretas < contagemVermelhas) {
-        return VERMELHA; // Jogador com as peças vermelhas vence
+      return VERMELHA; // Jogador com as peças vermelhas vence
     } else {
-        return NENHUM; // Empate
+      return NENHUM; // Empate
     }
-}
+  }
 
-void JogoDeDamas::encerrarJogo() {
-        exit(0);
-}
+  // Encerra o jogo
+  void encerrarJogo() {
+    exit(0);
+  }
+};
