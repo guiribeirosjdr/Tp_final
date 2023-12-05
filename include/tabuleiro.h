294d0ee47e761d237f2bd438127d6f2280528b11
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-12-05 17:57:30 -0300

    Update tabuleiro.h

--- include/tabuleiro.h
+++ include/tabuleiro.h
@@ -3,24 +3,44 @@
 
 #include <vector>
 #include "peca.h"
-#include "regrasdojogo.h"  
-#include "interfacegrafica.h" 
+#include "regrasdojogo.h"  
+#include "interfacegrafica.h" 
 
 using namespace std;
 
+// Representa o tabuleiro do jogo de damas
 class Tabuleiro {
 private:
-    std::vector<std::vector<Peca *>> casas;
-    Cor daVez;
+  // Representa as casas do tabuleiro
+  std::vector<std::vector<Peca *>> casas;
+
+  // Representa a cor do jogador que está na vez
+  Cor daVez;
 
 public:
-    Tabuleiro();
-    ~Tabuleiro();
-    void imprime();
-    bool movimenta(int linha, int coluna, bool diag_esq);
-    Cor getDaVez() const;
-    Peca* getPeca(int linha, int coluna) const;
-    void realizarJogada(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino);
+  // Construtor do tabuleiro
+  Tabuleiro();
+
+  // Destrutor do tabuleiro
+  ~Tabuleiro();
+
+  // Imprime o tabuleiro
+  void imprime();
+
+  // Move uma peça no tabuleiro
+  bool movimenta(int linha, int coluna, bool diag_esq);
+
+  // Obtém a cor do jogador que está na vez
+  Cor getDaVez() const;
+
+  // Obtém a peça na casa especificada
+  Peca* getPeca(int linha, int coluna) const;
+
+  // Realiza uma jogada no tabuleiro
+  void realizarJogada(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino);
+
+  // Verifica se uma jogada é válida
+  bool jogadaValida(int linhaOrigem, int colunaOrigem, bool diag_esq);
 };
 
 #endif
