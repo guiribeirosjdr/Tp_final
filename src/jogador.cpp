b7d4a0ef2dec10b1604454b8d9aa141d9f89eee3
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2023-12-05 18:04:43 -0300

    Update jogador.cpp

--- src/jogador.cpp
+++ src/jogador.cpp
@@ -1,10 +1,35 @@
 #include "jogador.h"
 #include "interfacegrafica.h"
 
-Jogador::Jogador(Cor cor) : cor(cor) {
-   }
+// Representa um jogador no jogo de damas
+class Jogador {
+private:
+  // A cor do jogador
+  Cor cor;
 
-Jogada Jogador::realizarJogada(Tabuleiro& tabuleiro) {
+public:
+  // Construtor do jogador
+  Jogador(Cor cor) : cor(cor) {
+    // Cria um jogador com a cor especificada
+  }
+
+  // Realiza uma jogada no tabuleiro
+  Jogada realizarJogada(Tabuleiro& tabuleiro) {
+    // Captura a jogada do jogador usando a interface gráfica
+
+    // Cria uma interface gráfica
     InterfaceGrafica interface;
-        return Jogada(/*coordenadas da jogada*/); // Substitua isso com as coordenadas reais da jogada
-}
+
+    // Captura a jogada
+    Jogada jogada = interface.capturarJogada(*this);
+
+    // Verifica se a jogada é válida
+    if (!tabuleiro.jogadaValida(jogada)) {
+      // A jogada é inválida, então retorna uma jogada inválida
+      return Jogada(-1, -1);
+    }
+
+    // A jogada é válida, então retorna a jogada
+    return jogada;
+  }
+};
